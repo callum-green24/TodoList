308c27d4ba2fafdf610896b03ae4d932e64c969e
Jeevan Miranda <158778751+jeevan-miranda@users.noreply.github.com>
2024-05-17 11:01:28 +1200

    task list showing

--- client/components/ToDo.tsx
+++ client/components/ToDo.tsx
@@ -1,13 +1,20 @@
-import { useQuery } from "@tanstack/react-query"
-import { fetchToDo } from "../apis/todo"
+import { useQuery } from '@tanstack/react-query'
+import { fetchToDoTasks } from '../apis/todo'
+// import { useParams } from 'react-router-dom'
 
-export default ToDo() {
-  const {data, isLoading, isError, error} = useQuery({queryKey: ['toDo'], queryFn: () => fetchToDo()})
+export default function ToDo() {
+  // const { tasks } = useParams()
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['toDo'],
+    queryFn: () => fetchToDoTasks(),
+  })
 
   if (isLoading) {
-    return (<>
-    <h3>Loading</h3>
-    </>)
+    return (
+      <>
+        <h3>Loading</h3>
+      </>
+    )
   }
 
   if (isError) {
@@ -17,9 +24,12 @@ export default ToDo() {
     return (
       <div>
         <h2>To Do List</h2>
-
+        <div>
+          {data.items.map((tasks) => (
+            <div key={tasks.id}>{tasks.task}</div>
+          ))}
+        </div>
       </div>
     )
   }
-
 }
